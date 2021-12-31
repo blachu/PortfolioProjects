0e25ea28cf26c25d4e9f5c4df8ab9c78cbeb94fb
Blazej H <60814043+hinc-b@users.noreply.github.com>
2021-12-31 17:05:49 +0100

    Update SQL_PCovid_DataExploration.sql
    same as last time with upper letters

--- SQL_PCovid_DataExploration.sql
+++ SQL_PCovid_DataExploration.sql
@@ -16,14 +16,14 @@ WHERE continent IS NOT NULL
 ORDER BY 1,2
 
 -- TOTAL CASES VERSUS TOTAL DEATHS: likelihood of dying if you contract covid in your country
-SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
+SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 AS DeathPercentage
 FROM PortfolioProject..CovidDeaths
 WHERE location like 'Poland'
 -- WHERE continent IS NOT NULL
 ORDER BY 1,2
 
 -- TOTAL CASES VERSUS POPULATION: percentage of population infected with covid
-SELECT location, date, population, total_cases, (total_cases/population)*100 as PercentPopulationInfected
+SELECT location, date, population, total_cases, (total_cases/population)*100 AS PercentPopulationInfected
 FROM PortfolioProject..CovidDeaths
 WHERE continent IS NOT NULL
 ORDER BY 1,2
