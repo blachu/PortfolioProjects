66b1a86e021434ce6d16ff4ff9a419cafcde521d
Blazej H <60814043+hinc-b@users.noreply.github.com>
2022-01-02 18:33:47 +0100

    Update SQLQuery_P1.sql
    upper letter changes

--- SQLQuery_P1.sql
+++ SQLQuery_P1.sql
@@ -5,15 +5,15 @@ TABLEAU PROJECT - Queries
 */
 
 -- 1
-SELECT SUM(new_cases) as Total_Cases, 
-	   SUM(new_deaths) as Total_Deaths, 
-	   SUM(new_deaths) / SUM(new_cases)*100 as DeathPercentage
+SELECT SUM(new_cases) AS Total_Cases, 
+	   SUM(new_deaths) AS Total_Deaths, 
+	   SUM(new_deaths) / SUM(new_cases)*100 AS DeathPercentage
 FROM PortfolioProject..CovidDeaths
-WHERE continent is not null 
+WHERE continent IS NOT null 
 ORDER BY 1,2
 
 -- 2
-SELECT location, SUM(new_deaths) as TotalDeathCount
+SELECT location, SUM(new_deaths) AS TotalDeathCount
 FROM PortfolioProject..CovidDeaths
 WHERE continent IS NULL
 AND location NOT IN ('World', 'European Union', 'International', 'Upper middle income', 'High income', 'Lower middle income', 'Low income')
@@ -22,16 +22,16 @@ ORDER BY TotalDeathCount DESC
 
 -- 3
 SELECT location, population, 
-	   MAX(total_cases) as HighestInfectionCount,  
-	   Max((total_cases / population)) * 100 as PercentPopulationInfected
+	   MAX(total_cases) AS HighestInfectionCount,  
+	   Max((total_cases / population)) * 100 AS PercentPopulationInfected
 FROM PortfolioProject..CovidDeaths
 GROUP BY location, population
 ORDER BY PercentPopulationInfected DESC
 
 -- 4
 SELECT location, population, date, 
-	   MAX(total_cases) as HighestInfectionCount,  
-	   Max((total_cases / population)) * 100 as PercentPopulationInfected
+	   MAX(total_cases) AS HighestInfectionCount,  
+	   Max((total_cases / population)) * 100 AS PercentPopulationInfected
 FROM PortfolioProject..CovidDeaths
 GROUP BY location, population, date
 ORDER BY PercentPopulationInfected DESC
